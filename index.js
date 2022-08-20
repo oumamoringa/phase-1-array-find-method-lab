94a76c18dcc9ce4f2b9e974ea9d1b8c97ef6c579
oumamoringa <richard.ouma@student.moringaschool>
2022-08-20 18:03:02 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // code your solution here
+function superbowlWin(record){
+    const result = record.find( record => record.result ==="W");
+//returns undefined if the record has no win objects
+    return !! result ? result.year : undefined;
+}
+
+
